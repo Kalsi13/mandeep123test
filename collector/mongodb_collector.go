9a782ab203f2b58a103157e4674be183ede9cc43
Kalsi13 <mandeep.kalsi@bskyb.com>
2017-02-10 16:43:56 +0000

    Update mongodb_collector.go

--- collector/mongodb_collector.go
+++ collector/mongodb_collector.go
@@ -1,9 +1,9 @@
 package collector
 
 import (
-	"github.com/Percona-Lab/prometheus_mongodb_exporter/shared"
-	"github.com/Percona-Lab/prometheus_mongodb_exporter/collector/mongod"
-	"github.com/Percona-Lab/prometheus_mongodb_exporter/collector/mongos"
+	"github.com/Kalsi13/mandeep123test/shared"
+	"github.com/Kalsi13/mandeep123test/collector/mongod"
+	"github.com/Kalsi13/mandeep123test/collector/mongos"
 	"github.com/golang/glog"
 	"github.com/prometheus/client_golang/prometheus"
 	"gopkg.in/mgo.v2"
