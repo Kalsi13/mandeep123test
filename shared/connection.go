356917c6dbfeacf71a9cc1d03bb885f483881cde
Kalsi13 <mandeep.kalsi@bskyb.com>
2017-02-10 16:41:55 +0000

    Added SessionServerVersion and NodeType

--- shared/connection.go
+++ shared/connection.go
@@ -119,3 +119,34 @@ func enrichWithOwnChecks(conn *tls.Conn, tlsConfig *tls.Config) error {
 
 	return nil
 }
+
+func MongoSessionServerVersion(session *mgo.Session) (string, error) {
+	buildInfo, err := session.BuildInfo()
+	if err != nil {
+		glog.Errorf("Could not get MongoDB BuildInfo: %s!", err)
+		return "unknown", err
+	}
+	return buildInfo.Version, nil
+}
+
+func MongoSessionNodeType(session *mgo.Session) (string, error) {
+	masterDoc := struct {
+		SetName interface{} `bson:"setName"`
+		Hosts   interface{} `bson:"hosts"`
+		Msg     string      `bson:"msg"`
+	}{}
+	err := session.Run("isMaster", &masterDoc)
+	if err != nil {
+		glog.Errorf("Got unknown node type: %s", err)
+		return "unknown", err
+	}
+
+	if masterDoc.SetName != nil || masterDoc.Hosts != nil {
+		return "replset", nil
+	} else if masterDoc.Msg == "isdbgrid" {
+		// isdbgrid is always the msg value when calling isMaster on a mongos
+		// see http://docs.mongodb.org/manual/core/sharded-cluster-query-router/
+		return "mongos", nil
+	}
+	return "mongod", nil
+}
